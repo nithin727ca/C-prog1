832263c5f4e242ac3c6982a0378052ef82f3b3df
nithin727ca <nithincyril27@gmail.com>
2025-10-26 22:13:20 +0530

    prog5

---
+++ .vscode/prog5.cpp
@@ -0,0 +1,22 @@
+//largest of three numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers: "<<endl;
+    cin>>a>>b>>c;
+    if(a>=b && a>=c)
+    {
+        cout<<a<<"is the largest number"<<endl;
+
+    }else if(b>=a && b>=c)
+    {
+        cout<<b<<"is the largest number"<<endl;
+
+    }else{
+        cout<<c<<"is the largest number"<<endl;
+    }
+    return 0;
+
+}
