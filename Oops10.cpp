59c78608a14ed48f1d3c7fee04deadaef9d93a06
nithin727ca <nithincyril27@gmail.com>
2025-11-02 10:19:10 +0530

    Oops10.cpp

---
+++ Oops10.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    int rollno;
+    string name;
+
+public:
+    // Constructor — same name as class, no return type
+    Student()
+    {
+        cout << "Constructor is called!" << endl;
+        rollno = 0;
+        name = "Unknown";
+    }
+
+    void getData()
+    {
+        cout << "Enter roll number: ";
+        cin >> rollno;
+        cout << "Enter name: ";
+        cin >> name;
+    }
+
+    void display()
+    {
+        cout << "Roll No: " << rollno << endl;
+        cout << "Name: " << name << endl;
+    }
+};
+
+int main()
+{
+    cout << "Creating object s1...\n";
+    Student s1; 
+
+    cout << "\nDefault values after constructor call:\n";
+    s1.display();
+
+    cout << "\nEnter new details:\n";
+    s1.getData();
+
+    cout << "\nAfter updating details:\n";
+    s1.display();
+
+    return 0;
+}
